34371e5f4a52a98fcba72e7e72d626eab544a584
yankai-lu <157673194+yankai-lu@users.noreply.github.com>
2024-12-14 21:08:27 +0800

    Update CriminalSystem.h

--- CriminalSystem/CriminalSystem.h
+++ CriminalSystem/CriminalSystem.h
@@ -3,10 +3,13 @@
 
 #include<iostream>
 #include<vector>
+#include<map>
 using namespace std;
 
 namespace CriminalSystem{
     class Criminal;
+    class RB_Tree;
+    
     class CrimeRecord{
         private:
             string type;
@@ -40,7 +43,7 @@ namespace CriminalSystem{
             void setCriminal(Criminal* other);
             friend ostream& operator <<(ostream& out,const CrimeRecord& cr);
     };
-
+    typedef CrimeRecord Node;
     class Person{
         private:
             string id;
@@ -71,11 +74,36 @@ namespace CriminalSystem{
         public:
             Criminal();
             Criminal(string i,string bir,string g,string loca,string n);
+            ~Criminal();
             void addcrimeRecord(CrimeRecord* record);
             void deletecrimeRecord(CrimeRecord* record);
             void outputAllinfo();
             void outputAllRecord();
-            CrimeRecord* getRecord_time(vector<int> t);
+            bool DeleteCrimeRecord(map<string,int>& treeNumber,RB_Tree* crimeTree, vector<int>& t);
+            void DeleteAllCrimeRecord(map<string,int>& treeNumber,RB_Tree* crimeTree);
+            //CrimeRecord* getRecord_time(vector<int> t);
+    };
+
+    class RB_Tree{
+            Node* root;
+            Node* nil;
+            void _leftRotate(RB_Tree& T, Node* x);
+            void _rightRotate(RB_Tree& T, Node* x);
+            void Transplant(RB_Tree& T, Node* z, Node* y);
+            void InsertFixup(RB_Tree& T, Node* x);
+            void DeleteFixup(RB_Tree& T, Node* x);
+            Node* TreeMinimum(Node* r);
+            void display(int* months);
+            vector<Node*> Interval(Node* node, int time1, int time2);
+        public:
+            RB_Tree(void){
+                nil = new Node(1,nil,nil,nil);
+                root = nil;
+            }
+            void Insert(RB_Tree& T, Node* z);
+            void Delete(RB_Tree& T, Node* z);
+            vector<Node*> SearchTime(int year, int month, int day, int hour, int minute);
+            vector<Node*> SearchInterval(int year1, int month1, int day1, int hour1, int minute1, int year2, int month2, int day2, int hour2, int minute2);
     };
 }
 #endif
